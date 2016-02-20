1a51725b29cb063de8fecdee9fb20905f1c19769
jmetz2131 <jacob.metz@wustl.edu>
2016-02-20 12:11:00 -0600

    Finished Question 1, Starting Question 2
    As stated in the code, I created the object which_door to show which
    door a contestant would choose and assigned it to class door. For the
    second question, I created the method PlayGame and I am currently
    figuring out how best to write the contents of the method.

--- PS3 - Jacob Metz.R
+++ PS3 - Jacob Metz.R
@@ -1,2 +1,17 @@
 ###Problem Set 3
-##Question 1 - Monty Hall Problem
+##Question 1
+##Defining the class door
+#The following code set which_door to be class "door".
+#The sample (with replacement) represents the door the contestant choose
+which_door <- structure(list(sample(1:3, 1, replace=T)), class="door")
+
+#This verifies that the class of the object which_door is class door.
+class(which_door)
+
+##Question 2
+#This is creating the method PlayGame for door objects
+door.PlayGame <- function() UseMethod("PlayGame")
+
+door.PlayGame = function(x){
+  
+}
